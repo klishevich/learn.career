8954a6e110bd88f4822546ad34fbc695bc514fa6
Michael Klishevich <busation@gmail.com>
2018-02-06 00:02:22 +0300

    fix fonts

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+  <link href="http://allfont.net/allfont.css?fonts=pt-sans" rel="stylesheet" type="text/css" />
   <?php wp_head(); ?>
 </head>
 
